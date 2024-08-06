6f3f95226bfe5ce15315a15f474e3dfeac396ac4
Bruce Szudera Wienand <brucesw@umich.edu>
2024-08-05 23:26:27 -0400

    Implement one postgres repository method

--- api/postgres.go
+++ api/postgres.go
@@ -1,10 +1,9 @@
 package api
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
+	"tenbounce/repository"
 
 	_ "github.com/lib/pq"
 
@@ -22,24 +21,45 @@ docker run \
 postgres:16-alpine
 */
 
+// docker exec -it fa58d2a22133 sh
+
+// psql -d myPostgresDb -U postgresUser
+
+/*
+CREATE TABLE points (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	timestamp timestamp,
+	user_id uuid,
+	point_type_id uuid,
+	value real
+);
+*/
+
+/*
+INSERT INTO points (timestamp, user_id, point_type_id, value) VALUES
+(CURRENT_TIMESTAMP, '550e8400-e29b-41d4-a716-446655440000', '4e4b2b1c-5063-425a-a409-71b431068f78', 20.01),
+(CURRENT_TIMESTAMP, '550e8400-e29b-41d4-a716-446655440000', '4e4b2b1c-5063-425a-a409-71b431068f78', 12.01),
+(CURRENT_TIMESTAMP, '550e8400-e29b-41d4-a716-446655440000', '4e4b2b1c-5063-425a-a409-71b431068f78', 13.01),
+(CURRENT_TIMESTAMP, '550e8400-e29b-41d4-a716-446655440000', '4e4b2b1c-5063-425a-a409-71b431068f78', 14.01),
+(CURRENT_TIMESTAMP, '550e8400-e29b-41d4-a716-446655440000', '4e4b2b1c-5063-425a-a409-71b431068f78', 15.01),
+(CURRENT_TIMESTAMP, '123e4567-e89b-12d3-a456-426614174000', '4e4b2b1c-5063-425a-a409-71b431068f78', 20.02),
+(CURRENT_TIMESTAMP, '123e4567-e89b-12d3-a456-426614174000', '4e4b2b1c-5063-425a-a409-71b431068f78', 12.02),
+(CURRENT_TIMESTAMP, '123e4567-e89b-12d3-a456-426614174000', '4e4b2b1c-5063-425a-a409-71b431068f78', 13.02),
+(CURRENT_TIMESTAMP, '123e4567-e89b-12d3-a456-426614174000', '4e4b2b1c-5063-425a-a409-71b431068f78', 14.02),
+(CURRENT_TIMESTAMP, '123e4567-e89b-12d3-a456-426614174000', '4e4b2b1c-5063-425a-a409-71b431068f78', 15.02);
+*/
+
 func tempPostgresRoute(g *echo.Group) {
 	g.POST("/postgres", func(c echo.Context) error {
 
-		// TODO(bruce): mysql??
-		db, err := sql.Open("postgres",
-			"postgresUser:postgresPW@tcp(127.0.0.1:5455)/hello")
-		if err != nil {
-			log.Fatal(err)
-		}
+		var psqlInfo = "host=127.0.0.1 port=5455 user=postgresUser password=postgresPW dbname=postgresDB sslmode=disable"
 
-		err = db.Ping()
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, "ping db")
-		}
-		defer db.Close()
+		var p Repository = repository.NewPostgresRepository(psqlInfo)
 
-		fmt.Println("gotme")
-		return c.JSON(http.StatusOK, nil)
+		if points, err := p.ListPoints("550e8400-e29b-41d4-a716-446655440000"); err != nil {
+			return c.JSON(http.StatusInternalServerError, fmt.Errorf("list points: %w", err))
+		} else {
+			return c.JSON(http.StatusOK, points)
+		}
 	})
-
 }
