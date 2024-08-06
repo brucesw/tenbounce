74fd2a8e410338e6798d2d80bd715ca390bb0e45
Bruce Szudera Wienand <brucesw@umich.edu>
2024-08-05 22:27:13 -0400

    Stub Postgres repository implementation

--- repository/postgres.go
+++ repository/postgres.go
@@ -1,1 +1,29 @@
 package repository
+
+import (
+	"fmt"
+	"tenbounce/model"
+)
+
+type Postgres struct {
+}
+
+func NewPostgresRepository() *Postgres {
+	return &Postgres{}
+}
+
+func (r *Postgres) GetUser(userID string) (model.User, error) {
+	return model.User{}, fmt.Errorf("user '%s' not found", userID)
+}
+
+func (r *Postgres) ListPoints(userID string) ([]model.Point, error) {
+	return []model.Point{}, nil
+}
+
+func (r *Postgres) SavePoint(p *model.Point) error {
+	return nil
+}
+
+func (r *Postgres) ListPointTypes() ([]model.PointType, error) {
+	return []model.PointType{}, nil
+}
