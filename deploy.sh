82bed5fd54b3d5169ae722dff745c7caa7b6e1f4
Bruce Szudera Wienand <brucesw@umich.edu>
2024-08-22 17:27:14 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-docker build -t tenbounce-image .
+# docker build -t tenbounce-image .
 
-docker tag tenbounce-image us-central1-docker.pkg.dev/tenbounce-prod/tenbounce/tenbounce:$COMMIT_SHA
+# Get the current Git commit SHA
+COMMIT_SHA=$(git rev-parse --short HEAD)
+
+if git diff-index --quiet HEAD --; then
+  CLEAN="true"
+else
+  CLEAN="false"
+fi
+
+docker tag tenbounce-image us-central1-docker.pkg.dev/tenbounce-prod/tenbounce/tenbounce:$COMMIT_SHA-$CLEAN
 
 docker push us-central1-docker.pkg.dev/tenbounce-prod/tenbounce/tenbounce
