c7031462d6c104e4c21aed67d6e8f140fbf227b6
Bruce Szudera Wienand <brucesw@umich.edu>
2024-08-22 20:59:49 -0400

    Introduce deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,15 @@
 COMMIT_SHA=$(git rev-parse --short HEAD)
 
 if git diff-index --quiet HEAD --; then
-  CLEAN="true"
+    CLEAN="true"
 else
-  CLEAN="false"
+    CLEAN="false"
 fi
+
+docker build -t tenbounce-image .
 
 docker tag tenbounce-image us-central1-docker.pkg.dev/tenbounce-prod/tenbounce/tenbounce:$COMMIT_SHA-$CLEAN
 
 docker push us-central1-docker.pkg.dev/tenbounce-prod/tenbounce/tenbounce
+
+gcloud run deploy tenbounce --platform=managed --region=us-central1 --image=us-central1-docker.pkg.dev/tenbounce-prod/tenbounce/tenbounce:$COMMIT_SHA-$CLEAN
